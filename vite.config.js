b2eaf9b43ac12d597a2934063cb9e7d41cb3fd7c
cveditor <cveditorai@gmail.com-NOREPLY>
2025-03-04 19:05:21 +0100

    fix1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+ // vite.config.js
   server: {
-    port: 5173, // o la porta che usi
-  },
+    port: process.env.PORT || 4173,
+    host: '0.0.0.0'
+  }
+,
 });
+console.log('Render PORT:', process.env.PORT); // Controlla la porta assegnata
