51da66e14a013e7c8dc6684c52a2529cdb7fbe75
cveditor <cveditorai@gmail.com-NOREPLY>
2025-03-04 19:19:19 +0100

    fix1.1

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
  // vite.config.js
-  server: {
-    port: process.env.PORT || 4173,
-    host: '0.0.0.0'
-  }
+ server: {
+  port: process.env.PORT || 4173,
+  host: true
+},
+preview: {
+  port: process.env.PORT || 4173,
+  host: true,
+  allowedHosts: ["fronted-ai.onrender.com"] // Sostituisci con il tuo dominio Render
+}
 ,
 });
 console.log('Render PORT:', process.env.PORT); // Controlla la porta assegnata
