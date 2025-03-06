b1f06a2fd408c9c4c494fac784f129e80d0e53eb
cveditor <cveditorai@gmail.com-NOREPLY>
2025-03-06 22:11:25 +0100

    fix 2.5

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Consente a Render di accedere al server
-    port: process.env.PORT || 5173, // Usa la porta di Render o quella locale
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
   },
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT || 4173,
+    allowedHosts: ['fronted-ai.onrender.com'], // Aggiungi il tuo host Render qui
   },
 });
