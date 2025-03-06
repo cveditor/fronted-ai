637029fb3f14ff73e7857d67ecc5b62fe5057889
cveditor <cveditorai@gmail.com-NOREPLY>
2025-03-06 20:34:52 +0100

    fix login1.4

--- src/services/socket.jsx
+++ src/services/socket.jsx
@@ -1,12 +1,16 @@
 import { io } from 'socket.io-client';
 
-const token = sessionStorage.getItem('token');
+const socket = io(import.meta.env.VITE_BACKEND_URL, {
+  withCredentials: true,
+  transports: ['websocket', 'polling'],
+});
 
-const socket = io(import.meta.env.VITE_API_URL , {
-  auth: { token }, // Invia il token per autenticazione
-  reconnection: true,
-  reconnectionAttempts: 5,
-  reconnectionDelay: 3000,
+socket.on('connect', () => {
+  console.log('✅ Connesso a Socket.io:', socket.id);
+});
+
+socket.on('disconnect', () => {
+  console.log('❌ Disconnesso da Socket.io');
 });
 
 export default socket;
