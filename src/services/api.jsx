03f894f982a219c6836da789f21289f6517ce79d
cveditor <cveditorai@gmail.com-NOREPLY>
2025-03-06 22:14:00 +0100

    fix 2.5

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: 'https://backend-ai-pxw3.onrender.com',
   withCredentials: true,
 });
 
