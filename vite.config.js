0e06bb7f4b2397d1c73b61d58d79abd141c75dfb
cveditor <cveditorai@gmail.com-NOREPLY>
2025-03-06 22:05:04 +0100

    fix 2.5

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig({
+  plugins: [react()],
   server: {
-    port: 5173,
+    host: '0.0.0.0', // Consente a Render di accedere al server
+    port: process.env.PORT || 5173, // Usa la porta di Render o quella locale
   },
   preview: {
-    port: 4173,
-  },
-  build: {
-    outDir: 'dist',
-  },
-  define: {
-    'process.env': {},
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
   },
 });
