7e8564f2c080ed3c23f09ff1e4eb69f22d4d2590
cveditor <cveditorai@gmail.com-NOREPLY>
2025-03-06 22:32:48 +0100

    fix 2.5

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -14,7 +14,7 @@ const Register = () => {
     setSuccess('');
 
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/register', {
+      const res = await axios.post('https://backend-ai-pxw3.onrender.com/api/auth/register', {
         username,
         email,
         password,
